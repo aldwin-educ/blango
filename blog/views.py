1d94c5aaa8c231241e643a5e065df92ed7425c52
Aldwin Mamiit <aldwinm@yahoo.com>
2024-01-20 07:11:51 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,19 @@ from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
 
+#from django.views.decorators.cache import cache_page
+#from django.views.decorators.vary import vary_on_cookie
+
 import logging
 logger = logging.getLogger(__name__)
 
+#@cache_page(300)
+#@vary_on_cookie
 def index(request):
+    #from django.http import HttpResponse
+    #logger.debug("Index function is called!") # you should only see this the first time since subsequent functiokn call is not performed.  Or wait 5 mins
+    #return HttpResponse(str(request.user).encode("ascii"))
+
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
     
